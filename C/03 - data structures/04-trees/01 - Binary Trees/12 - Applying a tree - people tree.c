4df709a62112c8410173d1850daf26b9caabbe6f
Ícaro Pavan <icaro.pavan97@gmail.com>
2024-11-04 16:27:24 -0300

    stuff

--- C/03 - data structures/04-trees/01 - Binary Trees/12 - Applying a tree - people tree.c
+++ C/03 - data structures/04-trees/01 - Binary Trees/12 - Applying a tree - people tree.c
@@ -1,27 +1,29 @@
 /*
 
-Creating a function to remove nodes from the tree
-
-The removeNode function encompass the 3 removing situations:
-leaf nodes, 1 child nodes, and 2 children nodes.
+Building an algorithm to apply a binary tree
+over people data.
 
 */ 
 
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+	char nome[50];
+	long long int cpf; // chave da arvore
+	char data_nascimento[11];
+	char email[50];
+} Person;
+
 typedef struct Node {
-	int value;
+	Person person;
 	struct Node *left;
 	struct Node *right;
 } Node;
 
-Node* insert(Node *root, int value);
-Node* search(Node *root, int value);
-int height(Node *root);
-int countNodes(Node *root);
-int countLeaves(Node *root);
-Node* removeNode(Node *root, int value);
+Node* insert(Node *root, Person person);
+Node* search(Node *root, long long int cpf);
+Node* removeNode(Node *root, long long int cpf);
 
 void printPreOrder(Node *root);
 void printInOrder(Node *root);
@@ -31,90 +33,78 @@ int main() {
 	Node *root = NULL;
 	
 	Node *searchResult;
-	int option, value;
-	int tree_height, tree_nodes, tree_leaves;
+	Person person;
+	int option;
+	long long int temp_lli;
 	
 	while(1) {
 		printf("What would you like to do?\n");
-		printf("[1] Insert value\n[2] Print tree (all traversals)\n[3] Search a value\n[4] Remove a node\n[5] Check tree height\n[6] Count elements\n[7] Count leaves\n[8] Exit\nR: ");
+		printf("[1] Insert value\n[2] Print tree (all traversals)\n[3] Search a value\n[4] Remove a node\n[5] Exit\nR: ");
 		scanf("%i", &option);
+		fflush(stdin);
 		
 		switch(option) {
 			case 1:
-				printf("\nInform a value to insert into the tree: ");
-				scanf("%i", &value);
+				printf("\nPerson's name: ");
+				scanf("%49[^\n]", person.nome);
+				fflush(stdin);
 				
-				root = insert(root, value);
+				printf("\nPerson's CPF (only numbers): ");
+				scanf("%11lli", &person.cpf);
+				fflush(stdin);
+				
+				printf("\nPerson's birth date: ");
+				scanf("%10[^\n]", person.data_nascimento);
+				fflush(stdin);
+				
+				printf("\nPerson's e-mail: ");
+				scanf("%49[^\n]", person.email);
+				fflush(stdin);
+				
+				root = insert(root, person);
 				printf("\n");
 				break;
+				
 			case 2:
 				if(root != NULL) {
 					printf("\nPre Order: ");
 					printPreOrder(root);
-					printf("\n");
+					printf("\n-------------------\n\n");
 					printf("In Order: ");
 					printInOrder(root);
-					printf("\n");
+					printf("\n-------------------\n\n");
 					printf("Post Order: ");
 					printPostOrder(root);
+					printf("\n-------------------");
 					printf("\n\n");
 				}
 				else
 					printf("\nThe tree is empty. Insert something before printing.\n\n");
 				break;
 			case 3:
-				printf("\nInform the value to search: ");
-				scanf("%i", &value);
+				printf("\nInform a person's CPF to search: ");
+				scanf("%lli", &temp_lli);
 				
-				searchResult = search(root, value);
+				searchResult = search(root, temp_lli);
 				
 				if(searchResult == NULL)
 					printf("\nThe value isn't in the tree.\n\n");
-				else
-					printf("\nThe value was found!\n\n");
+				else {
+					printf("\nName: %s\n", searchResult->person.nome);
+					printf("CPF: %lli\n", searchResult->person.cpf);
+					printf("Birth Date: %s\n", searchResult->person.data_nascimento);
+					printf("E-mail: %s\n\n", searchResult->person.email);
+				}
+
 				break;
 			case 4:
-				printf("\nInsert the value to remove from the tree: ");
-				scanf("%i", &value);
+				printf("\nInsert a CPF to remove the person from the tree: ");
+				scanf("%lli", &temp_lli);
 				
-				root = removeNode(root, value);
+				root = removeNode(root, temp_lli);
 				
 				break;
 			case 5:
-				tree_height = height(root);
-				
-				if(tree_height == -1)
-					printf("\nYour tree is empty. Add something to verify the height.\n\n");
-				else if(tree_height == 0)
-					printf("\nThe tree height is 0. (your tree has only one element)\n\n");
-				else
-					printf("\nThe tree height is: %i\n\n", tree_height);
-				
-				break;
-			case 6:
-				tree_nodes = countNodes(root);
-				
-				if(tree_nodes == 0)
-					printf("\nThe tree has %i nodes. (the tree is empty)\n\n", tree_nodes);
-				else if(tree_nodes == 1)
-					printf("\nThe tree has %i node.\n\n", tree_nodes);
-				else
-					printf("\nThe tree has %i nodes.\n\n", tree_nodes);
-					
-				break;
-			case 7:
-				if(root != NULL) {
-					tree_leaves = countLeaves(root);
-				
-					if(tree_leaves == 1)
-						printf("\nThe tree has 1 leaf.\n\n");
-					else
-						printf("\nThe tree has %i leaves.\n\n", tree_leaves);
-				}
-				else
-					printf("\nThe tree is empty. Insert something to check the leaves number.\n\n");
-				break;
-			case 8:
 				printf("\nGoodbye!\n");
 				break;
 			default:
@@ -122,107 +112,82 @@ int main() {
 				break;
 		}
 		
-		if(option == 8)
+		if(option == 5)
 			break;
 	}
 	
 	return 0;
 }
 
-Node* insert(Node *root, int value) {
+Node* insert(Node *root, Person person) {
 	if(root == NULL) {
 		Node *new_node = (Node*)malloc(sizeof(Node));
 		
-		new_node->value = value;
+		new_node->person = person;
 		new_node->left = NULL;
 		new_node->right = NULL;
 		
-		printf("\nThe value was successfully inserted into the tree.\n");
+		printf("\nThe person was successfully inserted into the tree.\n");
 		
 		return new_node;
 	}
 	else {
-		if(value < root->value)
-			root->left = insert(root->left, value);
-		else if(value > root->value)
-			root->right = insert(root->right, value);
+		if(person.cpf < root->person.cpf)
+			root->left = insert(root->left, person);
+		else if(person.cpf > root->person.cpf)
+			root->right = insert(root->right, person);
 		else
-			printf("\nThe value has already been added to the tree.\n");
+			printf("\nThe person has already been added to the tree.\n");
 		
 		return root;
 	}
 }
 
-Node* search(Node *root, int value) {
+Node* search(Node *root, long long cpf) {
 	if(root != NULL) {
-		if(root->value == value)
+		if(root->person.cpf == cpf)
 			return root;
 		else {
-			if(value < root->value)
-				return search(root->left, value);
+			if(cpf < root->person.cpf)
+				return search(root->left, cpf);
 			else
-				return search(root->right, value);
+				return search(root->right, cpf);
 		}
 	}
 	
 	return NULL;
 }
 
-int height(Node* root) {
-	if(root == NULL)
-		return -1;
-	else {
-		int leftHeight = height(root->left);
-		int rightHeight = height(root->right);
-		
-		return 1 + (leftHeight > rightHeight? leftHeight: rightHeight);
-	}
-}
-
-int countNodes(Node *root) {	
-	return ((root == NULL)? 0: 1 + countNodes(root->left) + countNodes(root->right));
-}
-
-int countLeaves(Node *root) {
-	if(root == NULL)
-		return 0;
-	else {
-		if(root->left == NULL && root->right == NULL)
-			return 1;
-		else {
-			return countLeaves(root->left) + countLeaves(root->right);
-		}
-	}
-}
-
-Node* removeNode(Node *root, int value) {
+Node* removeNode(Node *root, long long int cpf) {
 	if(root == NULL) {
 		// nao achou o elemento
-		printf("\nThe node wasn't find in the tree.\n\n");
+		printf("\nThe person wasn't find in the tree.\n\n");
 		return NULL;
 	}
 	else {
-		if(root->value == value) {
+		if(root->person.cpf == cpf) {
 			// verificar o tipo do nó
 			if(root->left == NULL && root->right == NULL) {
 				// remover nó folha (sem filhos)
 				free(root);
-				printf("\nThe leaf element %i was removed successfully!\n\n", value);
+				printf("\nThe person (leaf element) identified by CPF %lli was removed successfully!\n\n", cpf);
 				return NULL;
 			}
 			else {
 				// verificar se nó possui um ou dois filhos
 				if(root->left != NULL && root->right != NULL) {
 					// remover nó com 2 filhos
+					Person person_found = root->person;
+					
 					Node *aux = root->left;
 					
 					while(aux->right != NULL)
 						aux = aux->right;
 					
-					root->value = aux->value;
-					aux->value = value;
+					root->person = aux->person;
+					aux->person = person_found;
 					
-					root->left = removeNode(root->left, value);
+					root->left = removeNode(root->left, cpf);
 					return root;
 				}
 				else {
@@ -230,17 +195,17 @@ Node* removeNode(Node *root, int value) {
 					Node *aux = (root->left != NULL)? root->left: root->right;
 					
 					free(root);
-					printf("\nThe element %i was removed successfully!\n\n", value);
+					printf("\nThe person identified by CPF %lli was removed successfully!\n\n", cpf);
 					return aux;
 				}
 			}
 		}
 		else {
 			// continua percorrendo a arvore
-			if(value < root->value)
-				root->left = removeNode(root->left, value);
+			if(cpf < root->person.cpf)
+				root->left = removeNode(root->left, cpf);
 			else
-				root->right = removeNode(root->right, value);
+				root->right = removeNode(root->right, cpf);
 			
 			return root;
 		}
@@ -249,7 +214,12 @@ Node* removeNode(Node *root, int value) {
 
 void printPreOrder(Node *root) {
 	if(root != NULL) {
-		printf("%i ", root->value);
+		printf("\n-------------------\n");
+		printf("Name: %s\n", root->person.nome);
+		printf("CPF: %lld\n", root->person.cpf);
+		printf("Birth Date: %s\n", root->person.data_nascimento);
+		printf("E-mail: %s", root->person.email);
+		
 		printPreOrder(root->left);
 		printPreOrder(root->right);
 	}
@@ -258,7 +228,13 @@ void printPreOrder(Node *root) {
 void printInOrder(Node *root) {
 	if(root != NULL) {
 		printInOrder(root->left);
-		printf("%i ", root->value);
+		
+		printf("\n-------------------\n");
+		printf("Name: %s\n", root->person.nome);
+		printf("CPF: %lld\n", root->person.cpf);
+		printf("Birth Date: %s\n", root->person.data_nascimento);
+		printf("E-mail: %s", root->person.email);
+		
 		printInOrder(root->right);
 	}
 }
@@ -267,6 +243,11 @@ void printPostOrder(Node *root) {
 	if(root != NULL) {
 		printPostOrder(root->left);
 		printPostOrder(root->right);
-		printf("%i ", root->value);
+		
+		printf("\n-------------------\n");
+		printf("Name: %s\n", root->person.nome);
+		printf("CPF: %lld\n", root->person.cpf);
+		printf("Birth Date: %s\n", root->person.data_nascimento);
+		printf("E-mail: %s", root->person.email);
 	}
 }
